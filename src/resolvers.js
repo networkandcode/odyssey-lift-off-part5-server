abd0dc19791fd78685d50bb935c6a80e9b8f3f21
ibrahis <shakir.ibrahim.ext@dematic.com>
2023-02-07 15:20:52 +0530

    add schema field durationInSeconds

--- src/resolvers.js
+++ src/resolvers.js
@@ -40,10 +40,13 @@ const resolvers = {
     author: ({ authorId }, _, { dataSources }) => {
       return dataSources.trackAPI.getAuthor(authorId);
     },
-
     modules: ({ id }, _, { dataSources }) => {
       return dataSources.trackAPI.getTrackModules(id);
     },
+    durationInSeconds: ({ length }) => length,
+  },
+  Module: {
+    durationInSeconds: ({ length }) => length,
   },
 };
 
